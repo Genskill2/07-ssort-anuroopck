72bfcef59b396ef2ba8b12134aaff53cde5f85a9
anuroopck <79464093+anuroopck@users.noreply.github.com>
2021-07-01 18:31:37 +0530

    Test 3

--- swap.c
+++ swap.c
@@ -1,9 +1,9 @@
-void swap_max(int arr[], int l,int n)
+void swap_max(int arr[], int l, int n)
 {
-	int max=0, *p;
+	int max=-100, *p;
 	for(int i=n; i<l; i++)
 	{
-		if (arr[i]>max)
+		if (arr[i] > max)
 		{
 			p = &arr[i];
 			max = arr[i];
@@ -12,9 +12,9 @@ void swap_max(int arr[], int l,int n)
 	*p = arr[n];
 	arr[n] = max;
 }
-void ssort(int arr[],int l)
+void ssort(int arr[], int l)
 {
-	for (int i=0;i<l;i++)
+	for (int i=0; i<l; i++)
 	{
 		swap_max(arr, l, i);
 	}
